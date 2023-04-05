246494fbc12d0aa69fa3234a0f21d798d487dc40
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-05 08:10:18 -0300

    fix: criptografando password ao atualizar senha #1

--- src/user_app/serializer.py
+++ src/user_app/serializer.py
@@ -19,3 +19,13 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password is not None:
+            user.set_password(password)
+            user.save()
+
+        return user
