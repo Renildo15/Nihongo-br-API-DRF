52ceea0902be4f4e04a8e290504cc0ba71b2c155
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-05 07:50:58 -0300

    fix: criptografando password #1

--- src/user_app/serializer.py
+++ src/user_app/serializer.py
@@ -5,3 +5,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name','last_name', 'username','email','password']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            username = validated_data['username'],
+            email = validated_data['email'],
+        )
+
+        user.set_password(validated_data['password'])
+
+        user.save()
+
+        return user
