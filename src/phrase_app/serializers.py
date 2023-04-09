588b6d725c18d1155fbb0bf7fa3b3d7c1d7c1af7
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-09 14:31:41 -0300

    refactor: finalizando #7

--- src/phrase_app/serializers.py
+++ src/phrase_app/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from user_app.serializer import UserSerializer
 from post_app.serializer import PostSerializer
+from post_app.models import Post
 from .models import Phrase
 
 
 class PhraseSerializer(serializers.HyperlinkedModelSerializer):
     user = UserSerializer(read_only=True)
-    post = PostSerializer(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
     class Meta:
         model = Phrase
         fields =['url', 'phrase', 'translation', 'create_at', 'update_at', 'post', 'user']
