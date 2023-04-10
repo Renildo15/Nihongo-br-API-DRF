ff74594c0059652d70741e202ec3d8d7578a59bb
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-10 11:09:02 -0300

    refactor: filtrsndo phrases por post #9

--- src/post_app/views.py
+++ src/post_app/views.py
@@ -1,8 +1,11 @@
 from .serializer import PostSerializer
 from .models import Post
+from phrase_app.models import Phrase
+from phrase_app.serializers import PhraseSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,5 +13,10 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     
+    @action(detail=True, methods=['get'])
+    def phrase_list(self, request, pk=None):
+        post =  self.get_object()
+        phrases = Phrase.objects.filter(post=post)
+        serializer = PhraseSerializer(phrases, many = True, context={'request': request})
 
-
+        return Response(serializer.data)
