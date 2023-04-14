64dc84403177f7adbde397223129a610eb794681
Renildo Rabil <renildorabi@ufrn.edu.br>
2023-04-14 16:51:53 -0300

    feat: listando posts por profile #9

--- src/user_app/views.py
+++ src/user_app/views.py
@@ -4,6 +4,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Profile
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from post_app.models import Post
+from post_app.serializer import PostSerializer
 
 
 class UserViewSets(viewsets.ModelViewSet):
@@ -22,3 +26,11 @@ class ProfileViewSet(viewsets.ModelViewSet):
         user = self.request.user
         queryset = Profile.objects.filter(user=user)
         return queryset
+    @action(detail=True, methods=['get'])
+
+    def post_list(self, request, pk=None):
+        profile = self.get_object()
+        post = Post.objects.filter(user=profile.user)
+        serializer = PostSerializer(post, many=True, context={"request":request})
+
+        return Response(serializer.data)
